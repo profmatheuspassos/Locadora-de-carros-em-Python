a947c3ff5fe5d33c1f0c2bc23823c76fbe6d4117
Matheus Passos <40068795+profmatheuspassos@users.noreply.github.com>
2024-03-03 10:12:05 +0200

    2024-03-04 Primeira Correção

--- locadora.py
+++ locadora.py
@@ -23,6 +23,19 @@ def opcaoInvalida():
     time.sleep(3)
     limpaTela()
 
+codigoCarros = [1, 2, 3, 4, 5, 6, 7, 8]
+
+carros = ["Chevrolet Tracker",
+          "Chevrolet Onix",
+          "Chevrolet Spin",
+          "Hyundai HB20",
+          "Hyundai Tucson",
+          "Fiat Uno",
+          "Fiat Mobi",
+          "Fiat Pulse"
+          ]
+
+"""
 carros = ["[1] Chevrolet Tracker - R$ 120/dia",
           "[2] Chevrolet Onix - R$ 90/dia",
           "[3] Chevrolet Spin - R$ 150/dia",
@@ -32,61 +45,73 @@ carros = ["[1] Chevrolet Tracker - R$ 120/dia",
           "[7] Fiat Mobi - R$ 70/dia",
           "[8] Fiat Pulse - R$ 130/dia"
           ]
+"""
+
+valores = [120, 90, 150, 85, 120, 60, 70, 130]
+
+limpaTela()
 
 while True:
-    limpaTela()
     opcao = mostraTitulo()
-    limpaTela()
+    totalCarros = len(carros)
     if opcao == 1:
+        if totalCarros == 0:
+            print("Todos os carros estão alugados. Verifique novamente amanhã.")
+            tchau()
+            break
+        limpaTela()
         print("===============================")
         print("Nosso portfólio de carros!")
         print("===============================")
-        for carro in carros:
-            print(carro)
+        for i in range(0, totalCarros):
+            print("[{}] {} - R$ {}/dia".format(codigoCarros[i], carros[i], valores[i]))
         print("===============================")
-        print("O que você deseja fazer? 0 - MENU PRINCIPAL | 1 - FINALIZAR O PROGRAMA")
+        print("O que você deseja fazer? 1 - MENU PRINCIPAL | 2 - FINALIZAR O PROGRAMA")
         decisao = int(input("Insira o número da opção desejada: "))
-        if decisao == 0:
+        if decisao == 1:
             limpaTela()
             mostraTitulo()
-        elif decisao == 1:
+        elif decisao == 2:
             tchau()
             break
         else:
             opcaoInvalida()
             continue
     elif opcao == 2:
+        if totalCarros == 0:
+            print("Todos os carros estão alugados. Verifique novamente amanhã.")
+            tchau()
+            break
+        limpaTela()
         print("===============================")
         print("Nosso portfólio de carros!")
         print("===============================")
-        for carro in carros:
-            print(carro)
+        for i in range(0,totalCarros):
+            print("[{}] {} - R$ {}/dia".format(codigoCarros[i], carros[i], valores[i]))
         print("===============================")
-        escolha = int(input("Indique o código do carro: "))
-        if escolha not in range(1, 9):
+        escolhaCarro = int(input("Indique o código do carro: "))
+        escolhaCarro -= 1
+        if escolhaCarro not in range(0, totalCarros):
             opcaoInvalida()
             continue
-        print("Carro escolhido: {}".format(carros[escolha - 1]))
+        print("Carro escolhido: {}".format(carros[escolhaCarro]))
         dias = int(input("Indique por quantos dias você quer alugar o carro: "))
-        if escolha not in range(1, 9):
-            opcaoInvalida()
-            continue
-        if escolha - 1 == 0:
-            print("Valor total do aluguel: R$ {:.2f}".format(float(dias * 120)))
-        elif escolha - 1 == 1:
-            print("Valor total do aluguel: R$ {:.2f}".format(float(dias * 90)))
-        elif escolha - 1 == 2:
-            print("Valor total do aluguel: R$ {:.2f}".format(float(dias * 150)))
-        elif escolha - 1 == 3:
-            print("Valor total do aluguel: R$ {:.2f}".format(float(dias * 85)))
-        elif escolha - 1 == 4:
-            print("Valor total do aluguel: R$ {:.2f}".format(float(dias * 120)))
-        elif escolha - 1 == 5:
-            print("Valor total do aluguel: R$ {:.2f}".format(float(dias * 60)))
-        elif escolha - 1 == 6:
-            print("Valor total do aluguel: R$ {:.2f}".format(float(dias * 70)))
+        if escolhaCarro == 0:
+            print("Valor total do aluguel: R$ {:.2f}".format(float(dias * valores[escolhaCarro])))
+        elif escolhaCarro == 1:
+            print("Valor total do aluguel: R$ {:.2f}".format(float(dias * valores[escolhaCarro])))
+        elif escolhaCarro == 2:
+            print("Valor total do aluguel: R$ {:.2f}".format(float(dias * valores[escolhaCarro])))
+        elif escolhaCarro == 3:
+            print("Valor total do aluguel: R$ {:.2f}".format(float(dias * valores[escolhaCarro])))
+        elif escolhaCarro == 4:
+            print("Valor total do aluguel: R$ {:.2f}".format(float(dias * valores[escolhaCarro])))
+        elif escolhaCarro == 5:
+            print("Valor total do aluguel: R$ {:.2f}".format(float(dias * valores[escolhaCarro])))
+        elif escolhaCarro == 6:
+            print("Valor total do aluguel: R$ {:.2f}".format(float(dias * valores[escolhaCarro])))
         else:
-            print("Valor total do aluguel: R$ {:.2f}".format(float(dias * 130)))
+            print("Valor total do aluguel: R$ {:.2f}".format(float(dias * valores[escolhaCarro])))
         print("Confirma o aluguel? 1 - SIM | 2 - NÃO")
         confirma = int(input("Indique a opção: "))
         if confirma not in [1, 2]:
@@ -94,16 +119,18 @@ while True:
             continue
         if confirma == 1:
             print("Aluguel confirmado!")
-            carros = carros.pop(escolha - 1)
+            codigoCarros.pop(escolhaCarro)
+            carros.pop(escolhaCarro)
+            valores.pop(escolhaCarro)
         else:
             print("Aluguel não confirmado!")
         print("===============================")
-        print("O que você deseja fazer? 0 - MENU PRINCIPAL | 1 - FINALIZAR O PROGRAMA")
+        print("O que você deseja fazer? 1 - MENU PRINCIPAL | 2 - FINALIZAR O PROGRAMA")
         decisao = int(input("Insira o número da opção desejada: "))
-        if decisao == 0:
+        if decisao == 1:
             limpaTela()
             mostraTitulo()
-        elif decisao == 1:
+        elif decisao == 2:
             tchau()
             break
         else:
